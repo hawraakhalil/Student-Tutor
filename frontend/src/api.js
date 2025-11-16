3fa372008a18764fa0c5e273efa47590b3b12544
hawraakhalil <hhk22@mail.aub.edu>
2025-11-16 17:33:51 +0200

    fixed api base

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://127.0.0.1:8000/api'
+const API_BASE = 'https://studenttutorrecommendationsystem-cmafddgndrc9cnf8.germanywestcentral-01.azurewebsites.net/api';
 
 async function fetchJson(url, options = {}){
   const res = await fetch(url, options)
